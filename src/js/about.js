eafffe360dbd42977d48ade19258fcac1901cef0
anargasimov1 <anar.gasimov.89@gmail.com>
2023-08-24 19:09:50 +0400

    v1.1

---
+++ src/js/about.js
@@ -0,0 +1,11 @@
+const bar = document.getElementById("bar"),
+    hidden = document.getElementById("hidden");
+
+
+let modal = false;
+
+bar.onclick = () => {
+    modal = !modal
+
+    modal ? hidden.style.height = "150px" : hidden.style.height = "0"
+}
