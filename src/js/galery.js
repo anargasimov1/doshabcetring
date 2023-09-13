019fd8b6082b1e67e4e8084be436e2124f92201a
anargasimov1 <anar.gasimov.89@gmail.com>
2023-09-13 19:21:58 +0400

    v.1.4

--- src/js/galery.js
+++ src/js/galery.js
@@ -1,4 +1,4 @@
-// hamburger menu
+
 
 const bar = document.getElementById("bar"),
     hidden = document.getElementById("hidden");
@@ -11,3 +11,27 @@ bar.onclick = () => {
 
     modal ? hidden.style.height = "150px" : hidden.style.height = "0"
 }
+
+const url = "https://meadow-silk-gauge.glitch.me/galery",
+    galery = document.getElementById("galery");
+
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => addelement(data))
+    .catch(err => galery.innerHTML = err)
+
+function addelement(par) {
+    for (let i = 0; i < par.length; ++i) {
+        galery.innerHTML += `
+        
+        <div class="card">
+            <img src=${par[i].image} alt="./">
+            <p></p>
+        </div>
+    
+        
+        
+        `
+    }
+}   
